ec7e8b3db78dfb17540d7fe77a743b0b27b8d658
AndyHu <meeasyhappy@gmail.com>
2015-07-02 14:43:53 +0800

    test

---
+++ baoBaoErGe/VideoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VideoViewController.swift
+//  baoBaoErGe
+//
+//  Created by andyhu on 15/7/2.
+//  Copyright (c) 2015年 andyhu. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
