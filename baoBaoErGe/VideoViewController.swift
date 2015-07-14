d5f83e47ab63faac87448b8682060d24ac503878
AndyHu <meeasyhappy@gmail.com>
2015-07-14 12:46:23 +0800

    No message.

--- baoBaoErGe/VideoViewController.swift
+++ baoBaoErGe/VideoViewController.swift
@@ -10,33 +10,48 @@ import UIKit
 import Persei
 import FlatUIColors
 
-class VideoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class VideoViewController: UIViewController{
 
+    @IBOutlet weak var menuButton1: UIButton!
+    @IBOutlet weak var menuButton: UIBarButtonItem!
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         let menu = MenuView()
         menu.backgroundColor = FlatUIColors.greenSeaColor()
+        self.title = "是的么"
         var item = MenuItem(image: UIImage(named: "video_40.png")!)
+       
         menu.items = [item, item,item]
-        tableView.addSubview(menu)
+        let navBar = self.navigationController!.navigationBar
+        navBar.barTintColor = FlatUIColors.greenSeaColor(); //UIColor(red: 65.0 / 255.0, green: 62.0 / 255.0, blue: 79.0 / 255.0, alpha: 1)
+        navBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
+        self.view.backgroundColor = UIColor(red: 65.0 / 255.0, green: 100.0 / 255.0, blue: 79.0 / 255.0, alpha: 1)
+        //tableView.addSubview(menu)
 
         // Do any additional setup after loading the view.
+    }
+//    
+//    @objc protocol GuillotineAnimationProtocol: NSObjectProtocol {
+//        func navigationBarHeight() -> CGFloat
+//        func anchorPoint() -> CGPoint
+//        func hostTitle () -> NSString
+//    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let destinationVC = segue.destinationViewController as! GuillotineMenuViewController
+        destinationVC.hostNavigationBarHeight = self.navigationController!.navigationBar.frame.size.height
+        destinationVC.hostTitleText = "是的"
+        destinationVC.view.backgroundColor = self.navigationController!.navigationBar.barTintColor
+        destinationVC.setMenuButtonWithImage(menuButton1.imageView!.image!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("game_videos", forIndexPath: indexPath) as! UITableViewCell
-        return cell
-    }
+
     
 
     /*
