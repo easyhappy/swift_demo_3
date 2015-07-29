2c1cb4d2566a7a6dff8426124f2542e1d7e32e3b
AndyHu <meeasyhappy@gmail.com>
2015-07-29 23:15:29 +0800

    No message.

--- baoBaoErGe/VideoViewController.swift
+++ baoBaoErGe/VideoViewController.swift
@@ -133,6 +133,7 @@ class VideoViewController: UIViewController, UITableViewDataSource, UITableViewD
         let statusbarHeight = UIApplication.sharedApplication().statusBarFrame.size.height
         //self.view.backgroundColor = UIColor(red: 65.0 / 255.0, green: 100.0 / 255.0, blue: 79.0 / 255.0, alpha: 1)
         menuButton1?.frame = CGRectMake(menuButtonPortraitLeadingConstant, menuButtonPortraitLeadingConstant+statusbarHeight, 30.0, 30.0)
+        menuButton1?.hidden = true
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
